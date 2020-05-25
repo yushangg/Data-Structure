35024fdc89b97a1b04976d916d4502d0c37ced8c
yushang <yushang_jg@163.com>
2020-05-25 21:07:15 +0800

    initialize stack & queue

---
+++ stack & queue/queue.cpp
@@ -0,0 +1,16 @@
+
+//first in first out
+int queue[maxSize];
+int front = 0, rear = 0;
+//In
+queue[++rear] = x;
+//out
+x = queue[++front];
+
+//circular queue
+//In 
+rear = (rear + 1) % maxSize;
+queue[rear] = x;
+//out
+front = (front +  1) % maxSize;
+x = queue[front];
