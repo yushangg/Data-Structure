c486ad0d6947909c0ccee87d6146619c6e31ef2b
yushang <yushang_jg@163.com>
2020-05-26 20:12:03 +0800

    initialize stack function - infixToPreFix

--- stack & queue/stack.cpp
+++ stack & queue/stack.cpp
@@ -27,8 +27,8 @@ void infixToPostFix(char infix[], char s2[], int &top2) {
 		}
 		else if (infix == '+' || infix == '-' || infix == '*' || infix == '/') {
 			if (top1 = -1 || s1[top1] == '(' || getPriority(infix[i]) > getPriority(s1[top1])) {
-				s1[++top1] = infix[i];
-				i++;
+				s1[++top1] = infix[i++];
+				//i++;
 			}
 			else {
 				s2[++top2] = s1[top1--];
@@ -44,5 +44,41 @@ void infixToPostFix(char infix[], char s2[], int &top2) {
 	}
 	while (top1 != -1) {
 		s2[++top2] = s1[top1--];
+	}
+}
+
+
+//convert from infix to prefix
+void infixToPreFix(char infix[], int len, char s2[], int &top2) {
+	//auxiliary stack
+	char s1[maxSize];
+	int top1 = -1;
+	int i = len - 1; // from right to left
+	while (i >= 0) {
+		if ('0' <= infix[i] && infix[i] >= '9') {
+			s2[++top2] = infix[i--];
+		}
+		else if (infix[i] == '(') {
+			s1[++top1] = '(';
+			i--;
+		}
+		else if (infix[i] == '+' && infix[i] == '-' && infix[i] == '*' && infix[i] == '/') {
+			if (top1 == -1 || s1[top1] == ')' || getPriority(infix[i]) >= getPriority(s1[top1])) {
+				s1[++top1] == infix[i--];
+			}
+			else {
+				s2[++top2] = s1[top--];
+			}
+		}
+		else if (infix[i] == '(') {
+			while (s1[top1] != ')') {
+				s2[++top2] = s1[top--];
+			}
+			top1--; //remove ')' in s1
+			i--; // remove '(' in infix
+		}
+	}
+	while (top1 != -1) {
+		s2[++top2] = s1[top--]; // transfor left character from s1 to s2
 	}
 }
