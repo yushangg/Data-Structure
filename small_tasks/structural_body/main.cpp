31a4d86b630a08d797e1a65893bcf6cee0a7f33b
yushang <yushang_jg@163.com>
2020-06-11 17:40:53 +0800

    correct small bug: change type of data

--- small_tasks/structural_body/main.cpp
+++ small_tasks/structural_body/main.cpp
@@ -1,40 +1,39 @@
 #include<stdio.h>
-#include<string>
 struct student {
 	char number[3];
-	int score1;
-	int score2;
-	int score3;
-	int sum;
-	int average;
+	double score1;
+	double score2;
+	double score3;
+	double sum;
+	double average;
 };
 int main() {
-	student s[5];
-	for (int i = 0; i < 5; i++) {
+	int i;
+	struct student s[5];
+	for (i = 0; i < 5; i++) {
 		scanf("%s", s[i].number);
-		scanf("%d", &s[i].score1);
-		scanf("%d", &s[i].score2);
-		scanf("%d", &s[i].score3);
+		scanf("%lf", &s[i].score1);
+		scanf("%lf", &s[i].score2);
+		scanf("%lf", &s[i].score3);
 		s[i].sum = s[i].score1 + s[i].score2 + s[i].score3;
 		s[i].average = s[i].sum / 3;
 	}
 	char *best_number;
 	best_number = s[0].number;
-	int best_sum = s[0].sum;
-	for(int i = 1; i < 5; i++) {
-		if (best_sum > s[i].sum) {
+	double best_sum = s[0].sum;
+	for(i = 1; i < 5; i++) {
+		if (best_sum < s[i].sum) {
 			best_number = s[i].number;
 			best_sum = s[i].sum;
 		}
 	}
 	printf("number score1 score2 score3 average\n");
-	for (int i = 0; i < 5; i++) {
-		printf("  %s      %d     %d     %d      %d\n",s[i].number,s[i].score1,s[i].score2,s[i].score3,s[i].average);
+	for (i = 0; i < 5; i++) {
+		printf("  %s    %.1lf   %.1lf   %.1lf    %.2lf\n",s[i].number,s[i].score1,s[i].score2,s[i].score3,s[i].average);
 	}
 	printf("The best student is ");
 	printf("%s",best_number);
 	printf(",sum=");
-	printf("%d", best_sum);
+	printf("%.1lf", best_sum);
+	return 0;
 }
-
-
