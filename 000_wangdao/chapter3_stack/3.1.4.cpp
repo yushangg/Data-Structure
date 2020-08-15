d03b862356195339358364d0f27c4d6ab8628122
yushang <yushang_jg@163.com>
2020-08-15 18:21:03 +0800

    15th Aug.

--- 000_wangdao/chapter3_stack/3.1.4.cpp
+++ 000_wangdao/chapter3_stack/3.1.4.cpp
@@ -19,3 +19,36 @@ bool Judge(char A[]) {
 	else
 		return false;
 }
+
+
+/*
+前半段的结点入栈后，和后半段的开始比较
+Q4
+*/
+typedef struct LNode {
+	char data;
+	struct LNode* next;
+}LNode, * LinkList;
+
+bool Judge(LinkList list, int n) {
+	int i; // 因为i是作为全局变量的，所以定义在最开始
+	char* s;
+	s = (char*)malloc(sizeof(char) * (n / 2));
+	LNode* p = list->next;
+	for (i = 0; i < n / 2; i++) {	//n / 2向下取整
+		*(s + i) = p->data;
+		p = p->next;
+	}
+	i--;	//恢复最后的i值，s[i]是最后一个存储的，结点个数是i - 0 + 1
+	/*当链表的结点个数是奇数个的时候，最中间的结点不参与比较*/
+	if (n % 2 == 1)
+		p = p->next;
+	while (p != NULL && p->data == *(s + i)) {
+		i--;
+		p = p->next;
+	}
+	if (i == -1)
+		return true;
+	else
+		return false;
+}
